6140e76673804948eaf3431b415a42cd03eeca2f
mradul13 <mradulchourasiya@gmail.com>
2024-06-13 01:49:47 +0530

    class name fixed

--- xcountriessearch/src/components/Card/Card.jsx
+++ xcountriessearch/src/components/Card/Card.jsx
@@ -4,9 +4,11 @@ import styles from "./Card.module.css";
 
 const Card = ({country})=>{
     return (
-        <div className={styles.countryCard}>
-            <FlagImage path={country["flags"]["svg"]} alt={country["flags"]["alt"]}/>
-        <h2 className={styles.country_name}>{country["name"]["common"]}</h2>
+        <div className="countryCard">
+            <div className={styles.countryCard}>
+                <FlagImage path={country["flags"]["svg"]} alt={country["flags"]["alt"]}/>
+            <h2 className={styles.country_name}>{country["name"]["common"]}</h2>
+            </div>
         </div>
     )
 }
